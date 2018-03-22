e9076a9bbf9032fbbc24b5d518198d20ba9706e2
Viraj Patel <patelviraj2@yahoo.com>
2018-03-22 17:18:32 -0400

    Added images , created UI, added random number generator

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var randomDiceIndex1 : Int = 0;
+    var randomDiceIndex2 : Int = 0;
 
+    @IBOutlet weak var diceImgView1: UIImageView!
+    @IBOutlet weak var diceImgView2: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib. // connected to github
@@ -20,6 +30,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Roll the Dicey button
+    @IBAction func rollBtn(_ sender: UIButton) {
+        
+        //arc4random_uniform function generates random number from 0 to 6 - 1
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex2)
+    }
+    
+    
+    
 }
 
