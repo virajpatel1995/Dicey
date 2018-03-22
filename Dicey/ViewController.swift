5931a601ae5212f9fb7e5f44abc8d562432fa69f
Viraj Patel <patelviraj2@yahoo.com>
2018-03-22 16:08:19 -0400

    Initial Commit

---
+++ Dicey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicey
+//
+//  Created by Viraj patel on 3/22/18.
+//  Copyright © 2018 Viraj patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
