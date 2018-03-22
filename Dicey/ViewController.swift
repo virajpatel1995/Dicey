77377ee380acccd86403751ff4619890a76cd95c
Viraj Patel <patelviraj2@yahoo.com>
2018-03-22 18:06:46 -0400

    Added motion to roll dice

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -58,6 +58,10 @@ class ViewController: UIViewController {
         
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImage()
+    }
+    
     
 }
 
